09527cd51f4722aa228ebc520239dc4c8b3659b0
Gabriel Oliveira Silva <gabriel.engsist@gmail.com>
2019-11-30 15:10:54 -0300

    Commit inicial projeto

---
+++ central-erros-backend/src/main/java/br/com/centralerros/centralerros/CentralerrosApplication.java
@@ -0,0 +1,13 @@
+package br.com.centralerros.centralerros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CentralerrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CentralerrosApplication.class, args);
+	}
+
+}
