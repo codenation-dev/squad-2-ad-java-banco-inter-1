5b0e080208e4aa70d751c209f5d719bc8b18a4e3
guilherassis <guilher.assis@gmail.com>
2019-12-03 22:20:08 -0300

    Implementado os endpoints do User abaixo;
    findByApplication - ok
    findByProfile - ok
    findByEmail - ok
    findByName - ok
    findAll - ok
    save  - ok
    update - ok
    delete (safe delete) - ok

--- central-erros-backend/src/main/java/br/com/centralerros/application/domain/repository/UserRepository.java
+++ central-erros-backend/src/main/java/br/com/centralerros/application/domain/repository/UserRepository.java
@@ -2,6 +2,7 @@ package br.com.centralerros.application.domain.repository;
 
 import br.com.centralerros.application.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -16,5 +17,18 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Transactional
     User findUserByEmailAndPassword (@Param("email") String email,
                                      @Param("password") String password);
+
+    @Transactional
+    @Query(value = "SELECT u.* FROM centralerros.user u, centralerros.user_application ua, centralerros.application a\n" +
+            "WHERE u.id = ua.user_id\n" +
+            "AND a.id = ua.application_id\n" +
+            "AND a.name = :name",nativeQuery = true)
+    User findByApplication (@Param("name") String name);
+
+    @Transactional
+    User findUserByProfile (@Param("profile") String profile);
+
+    @Transactional
+    User findUserByName (@Param("name") String name);
 
 }
