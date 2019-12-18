a7a9cc20e97d312ee1732a209b259b4036081b87
Gustavo Reis <gustavohdosreis@gmail.com>
2019-12-18 00:23:33 -0300

    Swagger Normal

--- central-erros-backend/src/main/java/br/com/centralerros/application/config/CorsFilter.java
+++ central-erros-backend/src/main/java/br/com/centralerros/application/config/CorsFilter.java
@@ -20,7 +20,7 @@ public class CorsFilter implements Filter {
         //response.setHeader("Access-Control-Allow-Origin", req.get);
         response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Allow-Headers",  "Authorization, Content-Type");
+        response.setHeader("Access-Control-Allow-Headers",  "*");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Expose-Headers", "Authorization");
         response.setHeader("Access-Control-Max-Age", "3600");
