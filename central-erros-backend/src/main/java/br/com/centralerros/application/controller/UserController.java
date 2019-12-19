8800bbfde5d3f8e97cd1acfcbc23e6b365d7ac19
guilherassis <guilher.assis@gmail.com>
2019-12-19 13:31:03 -0300

    Corrigido o endpoint de save, pois ele estava recebendo o userVo ao invez do user.

--- central-erros-backend/src/main/java/br/com/centralerros/application/controller/UserController.java
+++ central-erros-backend/src/main/java/br/com/centralerros/application/controller/UserController.java
@@ -1,7 +1,6 @@
 package br.com.centralerros.application.controller;
 
 import br.com.centralerros.application.domain.entity.User;
-import br.com.centralerros.application.domain.vo.UserVO;
 import br.com.centralerros.application.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,10 +16,9 @@ public class UserController extends BasicController{
 
     @CrossOrigin
     @PostMapping("/save")
-    public ResponseEntity save(@RequestBody UserVO userVO){
-        return ResponseEntity.ok(
-                utils.map(userService.save(utils.map(userVO, User.class)), UserVO.class)
-        );
+    public ResponseEntity save(@RequestBody User user){
+        return ResponseEntity.ok(userService.save(user));
+
     }
 
 
